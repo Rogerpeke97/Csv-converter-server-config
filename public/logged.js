448f7d3f2c2d556875fbae39d6e39eef4655eb0a
Ignacio <rogerpeke97@gmail.com>
2020-08-01 16:43:05 -0300

    Added commentary to all steps and share my server setup

---
+++ public/logged.js
@@ -0,0 +1,202 @@
+// it is the same as the app.js file but the only difference is that i removed
+// the document id of the login and signup
+document.getElementById('fileUp').addEventListener("change", () => {
+    const arty = document.getElementById('fileUp');
+    const fileList = arty.files[0].name;
+    JSON.stringify(fileList);
+    console.log(fileList)
+    let list = document.getElementById('aboutToUpload')
+    if (list.childNodes.length > 1) {
+        list.removeChild(list.childNodes[1])
+        let node = document.createElement("li");
+        let textnode = document.createTextNode(fileList);
+        node.appendChild(textnode);
+        document.getElementById('aboutToUpload').appendChild(node);
+    } else {
+        let node = document.createElement("li");
+        let textnode = document.createTextNode(fileList);
+        node.appendChild(textnode);
+        document.getElementById('aboutToUpload').appendChild(node);
+    }
+})
+document.getElementById('chart_title').addEventListener("click", () => {
+    document.getElementById('chart_title').value = ''
+})
+let xaxis = [];
+let yaxis = []
+let filename = ['']
+let first_row_multiplied = [];
+let big_chart_y = [];
+chartIt();
+async function chartIt() {
+    //function to check if file is uploaded
+    async function getData() {
+        if (filename.length >= 2) {
+            filename.splice(0, 1)
+            xaxis.splice(0, xaxis.length);
+            yaxis.splice(0, yaxis.length);
+            setTimeout(updateChart, 3500);
+            console.log("works")
+            console.log(filename)
+        } else {
+            console.log("im being called again")
+            console.log(filename)
+            const response = await fetch('test.csv');
+            const data = await response.text();
+            // '\n' = means line break or the row of the data, so it wll separate the data in rows
+            let rows = data.split('\n');
+            let first_row = rows.slice(0, 1);
+            first_row = first_row[0].split(',');
+            first_row.splice(0, 1);
+            rows = rows.slice(1);
+            for (i = 0; i < rows.length; i++) {
+                for (j = 0; j < first_row.length; j++) {
+                    first_row_multiplied.push([first_row[j]]);
+                }
+            }
+            console.log(first_row_multiplied);
+            rows.forEach((element) => {
+                rows = element.split(',');
+                let first_column = rows[0];
+                xaxis.push(first_column);
+                let g = []
+                for (i = 1; i < rows.length; i++) {
+                    g.push(parseFloat(rows[i]));
+                }
+                g = g.reduce((a, b) => a + b, 0) / g.length;
+                yaxis.push(g.toString());
+                let y = []
+                for (i = 1; i < rows.length; i++) {
+                    y.push(rows[i]);
+                }
+                big_chart_y.push(y);
+            });
+            console.log(yaxis);
+            big_chart_y = [].concat.apply([], big_chart_y)
+            console.log(big_chart_y);
+        }
+        async function updateChart() {
+            const response = await fetch('/uploaded');
+            const dataJson = await response.json();
+            const data = dataJson[0].name.toString();
+            let rows = data.split('\n');
+            big_chart_y = [];
+            first_row_multiplied = [];
+            let first_row = rows.slice(0, 1);
+            first_row = first_row[0].split(',');
+            first_row.splice(0, 1);
+            rows = rows.slice(1);
+            for (i = 0; i < rows.length; i++) {
+                for (j = 0; j < first_row.length; j++) {
+                    first_row_multiplied.push([first_row[j]]);
+                }
+            }
+            console.log(first_row_multiplied);
+            rows.forEach((element) => {
+                rows = element.split(',');
+                let first_column = rows[0];
+                xaxis.push(first_column);
+                let g = []
+                for (i = 1; i < rows.length; i++) {
+                    g.push(parseFloat(rows[i]));
+                }
+                g = g.reduce((a, b) => a + b, 0) / g.length;
+                yaxis.push(g.toString());
+                let y = []
+                for (i = 1; i < rows.length; i++) {
+                    y.push(rows[i]);
+                }
+                big_chart_y.push(y);
+            });
+            console.log(yaxis);
+            big_chart_y = [].concat.apply([], big_chart_y);
+            get_rgb();
+            myChart.update();
+        };
+    }
+    await getData()
+    //function goes after the other function calls so that i can reference xaxis
+    function rgbRandom(r, g, b, a) {
+        r = Math.floor(Math.random() * 255);
+        g = Math.floor(Math.random() * 255);
+        b = Math.floor(Math.random() * 255);
+        a = Math.random();
+        return "rgb(" + r + ", " + g + ", " + b + ", " + a + ")";
+    }
+    let backArr = [];
+    let chart_title = "FILL THIS WITH YOUR OWN CHART TITLE"
+    Chart.defaults.global.defaultFontColor = "#fff";
+    const ctx = document.getElementById('myChart').getContext('2d');
+    let configDefault;
+    configDefault = {
+        type: 'bar',
+        data: {
+            labels: xaxis,
+            datasets: [{
+                label: chart_title,
+                data: yaxis,
+                backgroundColor: backArr,
+                borderColor: 'rgb(0, 0, 0)',
+                borderWidth: 1,
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                yAxes: [{
+                    ticks: {}
+                }]
+            }
+        }
+    };
+    get_rgb();
+
+    function get_rgb() {
+        if (backArr.length > 1) {
+            for (let i = 0; i < configDefault.data.labels.length; i++) {
+                backArr.push(rgbRandom([i])); //to push into the array
+            }
+        } else {
+            for (let i = 0; i < configDefault.data.labels.length; i++) {
+                backArr.push(rgbRandom([i])); //to push into the array
+            }
+        }
+    }
+    let myChart = new Chart(ctx, configDefault); // here my chart ends
+    document.getElementById('uploadClick').addEventListener("click", () => {
+        setTimeout(() => {
+            let children = document.getElementById('aboutToUpload').childNodes[1].textContent;
+            if (children == undefined) {
+                console.log("NO FILEE")
+            } else {
+                filename.push(children)
+                console.log(children)
+                getData();
+                myChart.clear();
+            }
+        }, 1000);
+    })
+    document.getElementById('multiaxial').addEventListener("click", () => {
+        configDefault.type = 'line';
+        configDefault.data.datasets[0].fill = false;
+        myChart.update();
+    });
+    document.getElementById('vertical').addEventListener("click", () => {
+        configDefault.type = 'bar'
+        myChart.update();
+    })
+    document.getElementById('upload_chart_title').addEventListener("click", () => {
+        configDefault.data.datasets[0].label = document.getElementById('chart_title').value;
+        myChart.update();
+    })
+    document.getElementById('view_all').addEventListener('click', async () => {
+        console.log(big_chart_y);
+        configDefault.data.labels = first_row_multiplied;
+        configDefault.data.datasets[0].data = big_chart_y;
+        document.getElementById('grid4').style.gridTemplateColumns = "1fr";
+        document.getElementById('grid4').style.gridTemplateRows = "50% 50%";
+        get_rgb();
+        myChart.update();
+    });
+}
---
+++ server.js
@@ -0,0 +1,290 @@
+//There is an upload folder inside the public folder which was udes for testing purposes and you 
+//can use it too to store files locally on your computer if that is what you want to, you need to add
+// a destination file in the google cloud download function
+// in the official example they add that
+
+const express = require('express');
+
+const app = express();
+
+const fs = require('fs');
+
+const Multer = require('multer');
+
+const path = require('path')
+
+const dotenv = require('dotenv').config({
+     path: './.env'
+});
+
+const ejs = require('ejs');
+
+const passport = require('passport')
+
+const bcrypt = require('bcrypt')
+
+const {
+     Pool,
+     Client
+} = require('pg');
+
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT;
+
+const session = require('express-session')
+
+const FileStore = require('session-file-store')(session);
+
+const {
+     Storage
+} = require('@google-cloud/storage');
+
+const {
+     format
+} = require('util');
+
+
+// Instantiate a storage client
+
+LocalStrategy = require('passport-local').Strategy
+//Postgresql
+const client = new Client({
+     user: process.env.DATABASE_USER, // ALL this process.env.variable are stored in a separate file
+     host: process.env.DATABASE_HOST, //which you need to add to your .gitignore file
+     database: process.env.DATABASE, //contains sensible info to connect to your database
+     password: process.env.DATABASE_PASSWORD,
+     port: process.env.DATABASE_PORT,
+});
+
+const dbConfig = {
+     user: client.user,
+     password: client.password,
+     database: client.database,
+     host: client.host,
+     port: client.port,
+}
+
+const pool = new Pool(dbConfig) //Pooling so that different users can connect to the database
+pool.on('error', function (err) {
+     error('idle client error', err.message, err.stack)
+})
+
+const cookieParser = require('cookie-parser');
+
+const gc = new Storage({
+     keyFilename: path.join(__dirname, '/directory-example'), // You need to download the json file
+     //from google and save it to your project when you deploy it or if you want to use it locally
+     projectId: 'example-id' //project id of google cloud storage
+})
+gc.getBuckets().then(x => console.log(x)); // Check if it connects properly to your google cloud bucket
+
+const FilesBucket = gc.bucket('example-name-bucket') //It shows on the id part of the function above
+//or in the google cloud storage list
+const multer = Multer({
+     storage: Multer.memoryStorage(),
+     limits: {
+          fileSize: 10 * 1024 * 1024, // Limiting file up to 5mb
+     },
+});
+// in this variable the download file information is stored after being turned into a 
+//string. When you receive the downloaded file its served as a buffer
+let doate = [{
+     name: []
+}]
+
+function deleteDoate() {
+     // To delete the array content after you download and send the json to the client side
+     doate[0].name.length = 0
+}
+
+app.post('/uploadedfile', multer.single('myFile'), async (req, res, next) => {
+     if (!req.file) {
+          res.status(400).send('No file uploaded.');
+          return;
+     } else {
+          // Create a new blob in the bucket and upload the file data.
+          const blob = FilesBucket.file(req.file.originalname);
+          const blobStream = blob.createWriteStream();
+          blobStream.on('error', (err) => {
+               next(err);
+          });
+          blobStream.on('finish', () => {
+               // The public URL can be used to directly access the file via HTTP.
+               const publicUrl = format(
+                    `https://storage.googleapis.com/${FilesBucket.name}/${blob.name}`
+               );
+               res.status(200).send(publicUrl);
+          });
+          blobStream.end(req.file.buffer);
+          // I set a timeout because the file does not upload right away
+          //therefore you need to wait for the file to be uploaded and then download it  
+          setTimeout(function main(require, respond) {
+               let bucketName = 'your-bucket-name'
+               let srcFilename = req.file.originalname
+               async function downloadFile() {
+                    // Downloads the file
+                    let x = await FilesBucket.file(srcFilename).download();
+                    console.log(x)
+                    console.log(
+                         `gs://${bucketName}/${srcFilename} downloaded`
+                    );
+                    deleteDoate();
+                    const respondFile = x.toString('utf-8') //turning the buffer into string 
+                    //adn then psuhing it into doate array
+                    doate[0].name.push(respondFile);
+                    //getting the fetch request from the client side app.js or logged.js
+                    app.get('/uploaded', (req, response) => {
+                         response.header("Content-Type", 'application/json');
+                         response.send(doate); // sending the json to the client
+                    })
+               }
+               downloadFile().catch(console.error);
+          }, 2000);
+     }
+});
+// setting the engine so that express can render the ejs files
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
+
+app.use(cookieParser('Your-cookie-parser secret key goes heree or you can use process.env.SESSION'));
+
+app.use(bodyParser.urlencoded({
+     extended: false
+}))
+app.use(bodyParser.json())
+
+app.use(session({
+     store: new FileStore(),
+     secret: process.env.SESSION,
+     resave: false,
+     saveUninitialized: false,
+     cookie: {
+          httpOnly: true,
+          expires: 60000 // the ammount of time your key is valid
+     }
+}))
+
+
+app.use(passport.initialize())
+
+
+app.use(passport.session())
+
+client.connect();
+//logging out and destroying the session
+app.post('/log_out', (req, res) => {
+     req.session.destroy((err) => {
+          if (err) {
+               console.log(err)
+          } else {
+               console.log(session);
+               res.redirect('/')
+          }
+     })
+})
+// index2 contains an error message in which you either input and invalid user or password
+app.get('/login', (req, res) => {
+     res.render('index2')
+})
+app.listen(PORT, () => console.log("Server started on port " + PORT));
+//upload file multer
+// REGISTRATION
+app.post('/signup', async (req, res) => {
+     const hashedPassword = await bcrypt.hash(req.body.password, 10)
+     client.connect();
+     const text = 'insert into users (first_name, password) VALUES($1, $2)'
+     const values = [req.body.user, hashedPassword];
+     client.query(text, values, (err, response) => {
+          if (err) {
+               console.log(err);
+          } else {
+               res.redirect('/');
+          }
+     })
+})
+//all the console.logs give you some insight about what is going on
+//and i think it's valuable to leave them there 
+//as it was painful to pin-point certain debugging issues.
+app.post('/', (req, res, next) => {
+     passport.authenticate('local', (err, user, info) => {
+          console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
+          console.log(`req.user: ${JSON.stringify(req.user)}`)
+          req.login(user, (err) => {
+               console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`);
+               if (req.user === null) {
+                    console.log(`req.user: ${JSON.stringify(req.user)}`)
+                    res.redirect('/login')
+               } else {
+                    res.redirect('/');
+               }
+          })
+     })(req, res, next);
+})
+// if the user it's not authenticated you go to the main page again, otherwise you
+//get the logged in website
+app.get('/', (req, res) => {
+     if (req.isAuthenticated()) {
+          let user = {
+               name: req.user.rows[0].first_name
+          }
+          res.render('index3.ejs', {
+               user
+          })
+     } else {
+          res.render('index.ejs')
+     }
+})
+//postgresql and session creation
+passport.use(new LocalStrategy((username, password, cb) => {
+     client.query('SELECT * FROM users where first_name = $1', [username], (err, result) => {
+          console.log(result);
+          if (err) {
+               console.log(err);
+               return cb(err);
+          }
+          if (result.rowCount === 1) {
+               const first = result.rows[0];
+               bcrypt.compare(password, first.password, (err, res) => {
+                    console.log(first.password)
+                    if (res) {
+                         console.log("Correct")
+                         cb(null, {
+                              id: first.id,
+                              username: first.first_name
+                         })
+                    } else {
+                         console.log("Incorrect password")
+                         console.log(first.id)
+                         return cb(null, false)
+                    }
+               })
+          }
+          //basically if there is 0 user was not found
+          if (result.rowCount === 0) {
+               console.log("No user with that name")
+               return cb(null, false)
+          }
+     })
+}))
+passport.serializeUser((user, done) => {
+     done(null, user.id);
+})
+
+passport.deserializeUser((id, done) => {
+     // and important point here, all the tutorials do not usually add this explanation and perhaps
+     // it's not hard to figure but in the doc examples of passport, the setup is different
+     // that exaple does not connect to a database, therefore in this part you should 
+     // query using the appropiate config of your database, i am using postgresql
+     client.query('SELECT * FROM users where id = $1', [id], (err, user) => {
+          if (err) {
+               done(err, null);
+          } else if (!user) {
+               // no user found
+               done(null, false);
+          } else {
+               done(null, user);
+          }
+     })
+})
